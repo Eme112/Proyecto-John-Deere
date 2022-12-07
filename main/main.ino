41babe6c615d880b80e5d6451d904cb8e6fec525
Eme112 <eme_pedraza@hotmail.com>
2022-12-06 23:16:59 -0600

    CAN communicaiton

--- main/main.ino
+++ main/main.ino
@@ -1,10 +1,5 @@
 #include "Control/Control.h"
-
-#define ENA PB9
-#define IN1 PB8
-#define IN2 PB7
-#define LINEAR_RES PA7
-#define INTERRUPT_PIN PA0
+#include "CAN/CAN.h"
 
 Control control;
 
@@ -16,64 +11,8 @@ void interruptHandler() {
   }*/
 }
 
-// Read SetPoint via serial based on syntax (setpoint)
-float readSetpoint() {
-  float setpoint = 0;
-  float multiplier;
-  char curr_char;
-  int state = 0;
-  
-  while(Serial.available()) {
-    curr_char = Serial.read();
-    switch(state) {
-      case 0:
-        if(curr_char == '(') {
-          state = 1;
-        } else {
-          Serial.println("Invalid syntax, returning 50");
-          state = 3;
-        }
-        break;
-      case 1:
-        if(curr_char >= '0' && curr_char <= '9') {
-          setpoint *= 10;
-          setpoint += (curr_char - '0');
-        } else if(curr_char == '.') {
-          state = 2;
-          multiplier = 0.1;
-        } else if(curr_char == ')') {
-          return setpoint;
-        } else {
-          Serial.println("Invalid syntax, returning 50");
-          state = 3;
-        }
-        break;
-      case 2:
-        if(curr_char >= '0' && curr_char <= '9') {
-          setpoint += (curr_char - '0')*multiplier;
-          multiplier /= 10;
-        } else if(curr_char == ')') {
-          return setpoint;
-        } else {
-          Serial.println("Invalid syntax, returning 50");
-          state = 3;
-        }
-        break;
-      case 3:
-        while(Serial.available()) {
-          Serial.read();
-        }
-        return 50;
-        break;
-    }
-  }
-
-  return 50;
-}
-
 void setup() {
   Serial.begin(115200);
-  while (!Serial);
   
   pinMode(ENA, OUTPUT);
   pinMode(IN1, OUTPUT);
